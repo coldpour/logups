856abfe612349796c0767cad5ad145d1af46d90d
Mike Holm <coldpour@gmail.com>
2020-05-30 17:11:20 -0600

    add emotion

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
+/** @jsx jsx */
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import { css, jsx } from "@emotion/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header
+        css={css`
+          background-color: #282c34;
+          min-height: 100vh;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          font-size: calc(10px + 2vmin);
+          color: white;
+        `}
+      >
+        <img
+          src={logo}
+          css={css`
+            height: 40vmin;
+            pointer-events: none;
+
+            @keyframes App-logo-spin {
+              from {
+                transform: rotate(0deg);
+              }
+              to {
+                transform: rotate(360deg);
+              }
+            }
+            @media (prefers-reduced-motion: no-preference) {
+              animation: App-logo-spin infinite 20s linear;
+            }
+          `}
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          css={css`
+            color: #61dafb;
+          `}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
