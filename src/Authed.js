9a3424f38de8af6916aed758c4245f779515428c
Mike Holm <coldpour@gmail.com>
2020-05-31 01:01:49 -0600

    query and post reps

--- src/Authed.js
+++ src/Authed.js
@@ -1,8 +1,33 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
+import { useState, useEffect, useContext } from "react";
 import logo from "./logo.svg";
+import { db } from "./firebase";
+import UserContext from "./UserContext";
 
 export default () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [reps, setReps] = useState(null);
+  const { user } = useContext(UserContext);
+
+  useEffect(() => {
+    db.collection("reps")
+      .where("user", "==", user.uid)
+      .limit(50)
+      .onSnapshot((snapshot) => {
+        if (!snapshot.size) setReps(null);
+        else {
+          setReps(
+            snapshot.docs.reduce(
+              (nextReps, doc) => ({ ...nextReps, [doc.id]: doc.data() }),
+              {}
+            )
+          );
+        }
+      });
+  }, [user.uid]);
+
   return (
     <header
       css={css`
@@ -37,19 +62,72 @@ export default () => {
         className="App-logo"
         alt="logo"
       />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setLoading(true);
+          const { target } = e;
+          const count = parseInt(target.elements.reps.value, 10);
+          db.collection("reps")
+            .add({
+              count,
+              user: user.uid,
+              timestamp: new Date(),
+            })
+            .then((doc) => setReps((prev) => ({ ...prev, [doc.id]: doc })))
+            .catch(setError)
+            .finally(setLoading(false));
+        }}
         css={css`
-          color: #61dafb;
+          font-size: 20px;
         `}
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
       >
-        Learn React
-      </a>
+        <label>
+          <div>reps</div>
+          <input
+            name="reps"
+            css={css`
+              font-size: 20px;
+            `}
+            type="number"
+            min="1"
+            step="1"
+          />
+        </label>
+        <button
+          css={css`
+            font-size: 20px;
+          `}
+          type="submit"
+          disabled={loading}
+        >
+          log
+        </button>
+        {error && (
+          <pre
+            css={css`
+              color: red;
+            `}
+          >
+            {JSON.stringify(error, null, 2)}
+          </pre>
+        )}
+      </form>
+      {reps && (
+        <div>
+          <h1>reps:</h1>
+          <ul>
+            {Object.entries(reps).map(([id, { count, timestamp }]) => (
+              <li key={id}>
+                <ul>
+                  <li>{count}</li>
+                  <li>{JSON.stringify(timestamp, null, 2)}</li>
+                </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </header>
   );
 };
