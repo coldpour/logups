5ce1f21c5a0a62f080f5bbc982f9fbb344b7af34
Mike Holm <coldpour@gmail.com>
2020-06-01 22:30:04 -0600

    allow update of reps

--- src/Set.js
+++ src/Set.js
@@ -7,6 +7,7 @@ import SetsContext from "./SetsContext";
 export default ({ id }) => {
   const { sets } = useContext(SetsContext);
   const [loading, setLoading] = useState(false);
+  const [editing, setEditing] = useState(false);
   const { timestamp, count } = sets[id];
 
   return (
@@ -17,22 +18,54 @@ export default ({ id }) => {
         padding: 0.5em;
       `}
     >
-      <button
-        disabled={loading}
-        onClick={() => {
-          setLoading(true);
-          db.collection("reps")
-            .doc(id)
-            .delete()
-            .catch((err) => {
-              setLoading(false);
-            });
-        }}
-      >
-        X
-      </button>
+      {editing ? (
+        <button
+          disabled={loading}
+          onClick={() => {
+            setLoading(true);
+            db.collection("reps")
+              .doc(id)
+              .delete()
+              .catch((err) => {
+                setLoading(false);
+              });
+          }}
+        >
+          X
+        </button>
+      ) : null}
       <div>{timestamp.toDate().toLocaleTimeString()}</div>
-      <div>{count}</div>
+      {editing ? (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setLoading(true);
+            db.collection("reps")
+              .doc(id)
+              .update({ count: parseInt(e.target.elements.reps.value, 10) })
+              .finally(() => {
+                setLoading(false);
+                setEditing(false);
+              });
+          }}
+        >
+          <label>
+            <div>reps</div>
+            <input
+              name="reps"
+              defaultValue={count}
+              min="1"
+              step="1"
+              type="number"
+            />
+          </label>
+          <button disabled={loading} type="submit">
+            OK
+          </button>
+        </form>
+      ) : (
+        <button onClick={() => setEditing(true)}>{count}</button>
+      )}
     </li>
   );
 };
