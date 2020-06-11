9bbe7fad71698ce99b67febf392a926538df6d56
Mike Holm <coldpour@gmail.com>
2020-06-11 01:00:47 -0600

    test firebase rules
    * add firebase-tools
    
    * init firebase project
    
    * add tests
    
    * add netlify config
    
    * add emulate to test
    
    * add env
    
    * try explicit
    
    * string the bool
    
    * flip order
    
    * print env
    
    * kill java
    
    * kill harder
    
    * try other localhost
    
    * cache the things
    
    * remove tests
    
    * use plugin for prettier
    
    * separate plugins lines
    
    * try to not print everything to the error
    
    * fix prettier
    
    * add prettier ignore
    
    * unit test plugin
    
    * try with exec
    
    * try without this env var
    
    * check the cache before running tests
    
    * try home cache for firebase
    
    * work around netlify resolving
    
    * try homedir and log cacheDir

---
+++ plugins/netlify-plugin-firebase-cache/index.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const { cwd } = require("process");
+const { homedir } = require("os");
+
+const getCacheDir = () => {
+  const absoluteCacheDir = path.resolve(homedir(), ".cache/firebase/emulators");
+  const netlifyBuildPluginResolvesFrom = cwd();
+  return path.relative(netlifyBuildPluginResolvesFrom, absoluteCacheDir);
+};
+
+module.exports = {
+  async onPreBuild({ utils: { cache } }) {
+    console.log("cacheDir", cache.getCacheDir());
+    if (await cache.restore(getCacheDir())) {
+      console.log("Found cached Firebase emulators!");
+    } else {
+      console.log("No cached Firebase emulators found.");
+    }
+  },
+  async onPostBuild({ utils: { cache } }) {
+    console.log("cacheDir", cache.getCacheDir());
+    if (await cache.save(getCacheDir())) {
+      console.log("Cached the Firebase emulators to speed up future builds!");
+    } else {
+      console.log("No Firebase emulators found.");
+    }
+  },
+};
