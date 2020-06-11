79e4782f834fa84d29ef9eea3e35793cae1bbf7e
Mike Holm <coldpour@gmail.com>
2020-06-11 00:03:59 -0600

    work around netlify resolving

--- plugins/netlify-plugin-firebase-cache/index.js
+++ plugins/netlify-plugin-firebase-cache/index.js
@@ -1,17 +1,22 @@
 const path = require("path");
+const { cwd, env } = require("process");
 
-const cacheDirs = path.normalize(`~/.cache/firebase/emulators`);
+const getCacheDir = () => {
+  const absoluteCacheDir = path.resolve(env.HOME, ".cache/firebase/emulators");
+  const netlifyBuildPluginResolvesFrom = cwd();
+  return path.relative(netlifyBuildPluginResolvesFrom, absoluteCacheDir);
+};
 
 module.exports = {
   async onPreBuild({ utils: { cache } }) {
-    if (await cache.restore(cacheDirs)) {
+    if (await cache.restore(getCacheDir())) {
       console.log("Found cached Firebase emulators!");
     } else {
       console.log("No cached Firebase emulators found.");
     }
   },
   async onPostBuild({ utils: { cache } }) {
-    if (await cache.save(cacheDirs)) {
+    if (await cache.save(getCacheDir())) {
       console.log("Cached the Firebase emulators to speed up future builds!");
     } else {
       console.log("No Firebase emulators found.");
