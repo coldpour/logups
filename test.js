21bde97213cec66b72f882e605553c49630636ff
Mike Holm <coldpour@gmail.com>
2020-06-14 21:45:16 -0600

    bob can read his reps but alice cannot

--- test.js
+++ test.js
@@ -4,6 +4,23 @@ const firebase = require("@firebase/testing");
 
 const projectId = "coldpour-test-project";
 
+const alice = { uid: "alice", email: "alice@example.com" };
+const bob = { uid: "bob", email: "bob@example.com" };
+
+const validAliceReps = {
+  count: 20,
+  user: alice.uid,
+  timestamp: new Date(),
+};
+
+const validBobReps = {
+  count: 13,
+  user: bob.uid,
+  timestamp: new Date(),
+};
+
+const bobsFirstReps = "bobsFirst";
+
 beforeAll(async () => {
   const rulesContent = fs.readFileSync(
     path.resolve(__dirname, "firestore.rules"),
@@ -13,20 +30,18 @@ beforeAll(async () => {
     projectId,
     rules: rulesContent,
   });
+  const adminReps = firebase
+    .initializeAdminApp({
+      projectId,
+    })
+    .firestore()
+    .collection("reps");
+  await adminReps.doc(bobsFirstReps).set(validBobReps);
 });
 
 afterAll(() => {
   firebase.apps().forEach((app) => app.delete());
 });
-
-const alice = { uid: "alice", email: "alice@example.com" };
-const bob = { uid: "bob", email: "bob@example.com" };
-
-const validAliceReps = {
-  count: 1,
-  user: alice.uid,
-  timestamp: new Date(),
-};
 
 describe("when not logged in", () => {
   const db = firebase
@@ -40,28 +55,48 @@ describe("when not logged in", () => {
   });
 });
 
+describe("when logged in as bob", () => {
+  const bobReps = firebase
+    .initializeTestApp({
+      projectId,
+      auth: bob,
+    })
+    .firestore()
+    .collection("reps");
+
+  test("bob can read his existing reps", async () => {
+    await firebase.assertSucceeds(bobReps.doc(bobsFirstReps).get());
+  });
+});
+
 describe("when logged in as alice", () => {
-  const db = firebase
+  const aLiceDb = firebase
     .initializeTestApp({
       projectId,
       auth: alice,
     })
     .firestore();
-  const reps = db.collection("reps");
+  const aliceReps = aLiceDb.collection("reps");
 
   test("alice can CRUD reps for herself", async () => {
-    await firebase.assertSucceeds(reps.add(validAliceReps));
-    const snapshot = await reps.where("user", "==", alice.uid).get();
+    await firebase.assertSucceeds(aliceReps.add(validAliceReps));
+    const snapshot = await aliceReps.where("user", "==", alice.uid).get();
     const { docs } = snapshot;
     expect(docs.length).toBeGreaterThan(0);
     const [first] = docs;
     const { id } = first;
     const { count } = first.data();
     const newCount = count * 2;
-    await firebase.assertSucceeds(reps.doc(id).update({ count: newCount }));
-    const updatedDoc = await reps.doc(id).get();
+    await firebase.assertSucceeds(
+      aliceReps.doc(id).update({ count: newCount })
+    );
+    const updatedDoc = await aliceReps.doc(id).get();
     expect(updatedDoc.data().count).toEqual(newCount);
-    await firebase.assertSucceeds(reps.doc(id).delete());
+    await firebase.assertSucceeds(aliceReps.doc(id).delete());
+  });
+
+  test("alice cannot read bob's existing reps", async () => {
+    await firebase.assertFails(aliceReps.doc(bobsFirstReps).get());
   });
 
   [
@@ -76,6 +111,6 @@ describe("when logged in as alice", () => {
   ].forEach(([desc, argFn]) => {
     const arg = argFn(validAliceReps);
     test(`alice cannot create reps ${desc}: ${JSON.stringify(arg)}`, async () =>
-      await firebase.assertFails(reps.add(arg)));
+      await firebase.assertFails(aliceReps.add(arg)));
   });
 });
